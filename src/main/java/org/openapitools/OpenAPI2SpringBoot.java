eb131b3dd9c73b7ba4d95fc9f1d41f96e6a4a0d1
SwingGuy1024 <SwingGuy1024@yahoo.com>
2021-01-19 01:01:40 -0800

    Minor tinkering. Removed HATEOAS kludge after fixing bug. (Removed Actuator-docs library)

--- src/main/java/org/openapitools/OpenAPI2SpringBoot.java
+++ src/main/java/org/openapitools/OpenAPI2SpringBoot.java
@@ -1,7 +1,5 @@
 package org.openapitools;
 
-import java.util.LinkedList;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,12 +7,7 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
 
 /**
  * Caching Configuration Reference: https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/#why-spring-redis
@@ -30,7 +23,7 @@ import org.springframework.plugin.core.SimplePluginRegistry;
     "org.openapitools.framework.util",
     "org.openapitools.configuration"
 })
-public class OpenAPI2SpringBoot implements CommandLineRunner {//}, CachingConfigurer {
+public class OpenAPI2SpringBoot implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(OpenAPI2SpringBoot.class);
     public static final String MENU_ITEM_CACHE = "menuItems";
@@ -55,16 +48,5 @@ public class OpenAPI2SpringBoot implements CommandLineRunner {//}, CachingConfig
             return 10;
         }
 
-    }
-
-    // I got a strange error message saying "Parameter 0 of method linkDiscoverers 
-    // in org.springframework.hateoas.config.HateoasConfiguration req" (I'm not 
-    // using HATEOAS.) This link showed me how to fix it. 
-    // https://www.codeleading.com/article/27224372792/
-    @Bean
-    public LinkDiscoverers discoverers() {
-        List<LinkDiscoverer> plugins = new LinkedList<>();
-        plugins.add(new CollectionJsonLinkDiscoverer());
-        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
 }
